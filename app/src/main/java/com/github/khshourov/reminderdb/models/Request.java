50dc8e8cd9dae086f672fd05abf454f1cd8c19c2
Kamrul H Shourov <shourov.kamrul@gmail.com>
2024-11-27 23:04:30 +0600

    Add validation for Avro Request object

--- app/src/main/java/com/github/khshourov/reminderdb/models/Request.java
+++ app/src/main/java/com/github/khshourov/reminderdb/models/Request.java
@@ -4,7 +4,29 @@ import com.github.khshourov.reminderdb.avro.AvroRequest;
 import com.github.khshourov.reminderdb.exceptions.ValidationException;
 import com.github.khshourov.reminderdb.interfaces.Validator;
 
-public class Request extends AvroRequest implements Validator {
+public class Request implements Validator {
+  private final AvroRequest avroRequest;
+
+  private Request(AvroRequest avroRequest) {
+    this.avroRequest = avroRequest;
+  }
+
+  public static Request fromAvroRequest(AvroRequest avroRequest) {
+    return new Request(avroRequest);
+  }
+
+  public AvroRequest getAvroRequest() {
+    return this.avroRequest;
+  }
+
   @Override
-  public void validate() throws ValidationException {}
+  public void validate() throws ValidationException {
+    if (this.avroRequest.getType() < 0 || this.avroRequest.getType() > 255) {
+      throw new ValidationException("type should be between 0 and 255");
+    }
+
+    if (this.avroRequest.getClientId().trim().isEmpty()) {
+      throw new ValidationException("clientId should not be empty");
+    }
+  }
 }
---
+++ app/src/test/java/com/github/khshourov/reminderdb/models/RequestTest.java
@@ -0,0 +1,101 @@
+package com.github.khshourov.reminderdb.models;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+import com.github.khshourov.reminderdb.avro.AvroRequest;
+import com.github.khshourov.reminderdb.exceptions.ValidationException;
+import java.nio.ByteBuffer;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+public class RequestTest {
+  private static final Integer VALID_TYPE = 1;
+  private static final String VALID_CLIENT_ID = "client-id";
+  private static final ByteBuffer VALID_PAYLOAD = ByteBuffer.wrap("payload".getBytes());
+
+  @ParameterizedTest
+  @MethodSource("validRequestType")
+  void requestTypeShouldBeBetween0And255(int requestType) {
+    AvroRequest avroRequest =
+        AvroRequest.newBuilder()
+            .setType(requestType)
+            .setClientId(VALID_CLIENT_ID)
+            .setPayload(VALID_PAYLOAD)
+            .build();
+    Request request = Request.fromAvroRequest(avroRequest);
+
+    assertDoesNotThrow(request::validate);
+  }
+
+  static Stream<Arguments> validRequestType() {
+    return Stream.of(arguments(0), arguments(1), arguments(128), arguments(255));
+  }
+
+  @ParameterizedTest
+  @MethodSource("invalidRequestType")
+  void validateMethodShouldThrowErrorWhenTypeIsNotBetween0And255(int invalidRequestType) {
+    AvroRequest avroRequest =
+        AvroRequest.newBuilder()
+            .setType(invalidRequestType)
+            .setClientId(VALID_CLIENT_ID)
+            .setPayload(VALID_PAYLOAD)
+            .build();
+    Request request = Request.fromAvroRequest(avroRequest);
+
+    assertThrows(ValidationException.class, request::validate);
+  }
+
+  static Stream<Arguments> invalidRequestType() {
+    return Stream.of(arguments(-1), arguments(256));
+  }
+
+  @ParameterizedTest
+  @MethodSource("invalidClientId")
+  void clientIdShouldNotBeEmpty(String invalidClientId) {
+    AvroRequest avroRequest =
+        AvroRequest.newBuilder()
+            .setType(VALID_TYPE)
+            .setClientId(invalidClientId)
+            .setPayload(VALID_PAYLOAD)
+            .build();
+    Request request = Request.fromAvroRequest(avroRequest);
+
+    assertThrows(ValidationException.class, request::validate);
+  }
+
+  static Stream<Arguments> invalidClientId() {
+    return Stream.of(arguments(""), arguments(" "), arguments("  "));
+  }
+
+  @Test
+  void payloadCanBeNull() {
+    AvroRequest avroRequest =
+        AvroRequest.newBuilder()
+            .setType(VALID_TYPE)
+            .setClientId(VALID_CLIENT_ID)
+            .setPayload(null)
+            .build();
+    Request request = Request.fromAvroRequest(avroRequest);
+
+    assertDoesNotThrow(request::validate);
+  }
+
+  @Test
+  void storedAvroRequestCanBeRetrieved() {
+    AvroRequest avroRequest =
+        AvroRequest.newBuilder()
+            .setType(VALID_TYPE)
+            .setClientId(VALID_CLIENT_ID)
+            .setPayload(VALID_PAYLOAD)
+            .build();
+    Request request = Request.fromAvroRequest(avroRequest);
+
+    assertEquals(avroRequest, request.getAvroRequest());
+  }
+}
